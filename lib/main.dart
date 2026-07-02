b53c00cd61d94279bfb622fb0b0b97748f97d1d2
Gagan C J <gaganjchandra2379@gmail.com>
2026-07-02 10:03:15 +0530

    chore: code cleanup and remove permission_handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'views/role_gate.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Permission.locationWhenInUse.request();
   runApp(const MyApp());
 }
 
